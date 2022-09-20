4007ab9af86fef2489db6979b7518d674f25a0b6
JHONMENDEZ2006 <110487167+JHONMENDEZ2006@users.noreply.github.com>
2022-09-20 12:34:00 -0500

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -26,5 +26,5 @@ let buttonMessage= {
 {buttonId: `${usedPrefix}donar`, buttonText: {displayText: '𝙳𝙾𝙽𝙰𝚁'}, type: 1}],
 'headerType': 6 }
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
-handler.command = ['Botiktok','Bottiktok']
+handler.command = ['botiktok','botiktok']
 export default handler
