dfac9be63b1ed5ff134b464f9f3674110299e88b
JHONMENDEZ2006 <110487167+JHONMENDEZ2006@users.noreply.github.com>
2022-10-01 21:51:11 -0500

    Update info-no_serbot.js

--- plugins/info-no_serbot.js
+++ plugins/info-no_serbot.js
@@ -2,33 +2,34 @@ let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
 let texto1 = `
-*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙰𝚂 𝙵𝚄𝙽𝙲𝙸𝙾𝙽𝙴𝚂 𝙿𝙰𝚁𝙰 𝚂𝙴𝚁 𝙱𝙾𝚃 (#𝚜𝚎𝚛𝚋𝚘𝚝, #𝚓𝚊𝚍𝚒𝚋𝚘𝚝, #𝚋𝚘𝚝𝚜, #𝚐𝚎𝚝𝚌𝚘𝚍𝚎, #𝚜𝚞𝚋𝚋𝚘𝚝𝚜, #𝚜𝚎𝚛𝚜𝚞𝚋𝚋𝚘𝚝) 𝙽𝙾 𝙴𝚂𝚃𝙰𝙽 𝙰𝙲𝚃𝚄𝙰𝙻𝙼𝙴𝙽𝚃𝙴 𝙵𝚄𝙽𝙲𝙸𝙾𝙽𝙰𝙻𝙴𝚂 𝙿𝙰𝚁𝙰 𝙴𝚂𝚃𝙴 𝙱𝙾𝚃 (𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝)*
-
-*—◉ 𝙿𝚄𝙴𝙳𝙴 𝙿𝚁𝙾𝙱𝙰𝚁 𝙸𝙽𝚂𝚃𝙰𝙻𝙰𝚁 𝙳𝙴𝚂𝙳𝙴 𝙲𝙴𝚁𝙾 𝙴𝙻 𝙱𝙾𝚃 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝙳𝙾 𝚄𝙽𝙾 𝙳𝙴 𝙻𝙾𝚂 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻𝙴𝚂 𝚀𝚄𝙴 𝙷𝙰𝚈 𝙴𝙽 𝙴𝙻 𝙲𝙰𝙽𝙰𝙻 𝙳𝙴 𝚃𝙷𝙴 𝚂𝙷𝙰𝙳𝙾𝚆 𝙱𝚁𝙾𝙺𝙴𝚁𝚂*
-*◉ https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA*`
+𝓬𝓻𝓮𝓪𝓭𝓸𝓻𝓮𝓼 𝓭𝓮 𝓮𝓼𝓽𝓮 𝓫𝓸𝓽
+𝓙𝓗𝓞𝓝𝓜𝓔𝓝𝓓𝓔𝓩
+𝗕𝗥𝗨𝗡𝗢 𝗦𝗢𝗕𝗥𝗜𝗡𝗢
+*—◉ P͓̽U͓̽E͓̽D͓̽E͓̽ ͓̽S͓̽E͓̽G͓̽U͓̽I͓̽R͓̽ ͓̽A͓̽L͓̽ ͓̽B͓̽O͓̽T͓̽ ͓̽E͓̽N͓̽ ͓̽Y͓̽O͓̽O͓̽T͓̽U͓̽B͓̽E͓̽ ͓̽D͓̽O͓̽N͓̽D͓̽E͓̽ ͓̽P͓̽R͓̽O͓̽X͓̽I͓̽M͓̽A͓̽M͓̽E͓̽N͓̽T͓̽E͓̽ ͓̽E͓̽L͓̽ ͓̽B͓̽O͓̽T͓̽ ͓̽E͓̽S͓̽T͓̽A͓̽R͓̽A͓̽ ͓̽P͓̽A͓̽R͓̽A͓̽ ͓̽T͓̽U͓̽ ͓̽U͓̽S͓̽O͓̽*
+*◉ https://www.youtube.com/channel/UCdPOQ4Wn6zNu8P3e98rXZwg*`
 let buttonMessage= {
 'document': { url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD` },
 'mimetype': `application/${document}`,
-'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
+'fileName': `「  F͓̽U͓̽C͓̽K͓̽ ͓̽Y͓̽O͓̽U͓̽ 」`,
 'fileLength': 99999999999999,
 'pageCount': 200,
 'contextInfo': {
 'forwardingScore': 200,
 'isForwarded': true,
 'externalAdReply': {
-'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
+'mediaUrl': 'https://www.youtube.com/channel/UCdPOQ4Wn6zNu8P3e98rXZwg',
 'mediaType': 2,
 'previewType': 'pdf',
-'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
+'title': 'βටͲ ϩ⊘ϩӠ,
 'body': wm,
 'thumbnail': imagen1,
-'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA' }},
+'sourceUrl': 'https://www.youtube.com/channel/UCdPOQ4Wn6zNu8P3e98rXZwg' }},
 'caption': texto1,
 'footer': wm,
 'buttons':[
-{buttonId: `${usedPrefix}instalarbot`, buttonText: {displayText: '𝙸𝙽𝚂𝚃𝙰𝙻𝙰𝚁𝙱𝙾𝚃'}, type: 1}],
+{buttonId: `${usedPrefix}instalarbot`, buttonText: {displayText: 'youtubebot'}, type: 1}],
 'headerType': 6 }
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })}
-handler.command = /^(jadibot|serbot|bots|subbots|getcode)/i
+handler.command = /^(jadibot|youtubebot|bots|subbots|getcode)/i
 export default handler
 
