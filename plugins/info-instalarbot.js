9e4799bb896e056e9377c49b1c67a8f16bbda8c3
JHONMENDEZ2006 <110487167+JHONMENDEZ2006@users.noreply.github.com>
2022-10-01 21:42:16 -0500

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,39 +1,25 @@
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let text = `
-*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙱𝙾𝚇𝙼𝙸𝙽𝙴 𝙷𝙾𝚂𝚃*
-> Tutorial: https://youtu.be/eC9TfKICpcY
-> Pagina Oficial: https://boxmineworld.com
-> Dashboard: https://dash.boxmineworld.com/home
-> Panel: https://panel.boxmineworld.com
-> Soporte: https://discord.gg/84qsr4v 
+*𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀
 
-------------------------------------
 
-*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙰𝙲𝙸𝙳𝙸𝙲𝙽𝙾𝙳𝙴𝚂 𝙷𝙾𝚂𝚃*
-> Tutorial: https://youtu.be/nbjvreJ0tUk
-> Pagina: https://billing.acidicnodes.ml/register?ref=ADII104p
-> Soporte: https://whatsapp.acidicnodes.com
+-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽
 
-------------------------------------
+░▒▓█ Ď𝕚ѕᖴ𝓻ᑌt𝒶 Ď𝓔ㄥ βỖ𝔱 █▓▒░
 
-*—◉ 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚃𝙴𝚁𝙼𝚄𝚇*
-> cd
-> termux-setup-storage
-> apt update 
-> pkg upgrade 
-> pkg install git -y
-> pkg install nodejs -y
-> pkg install ffmpeg -y
-> pkg install imagemagick -y
-> pkg install yarn
-> git clone https://github.com/BrunoSobrino/Mystic-termux
-> cd Mystic-termux
-> yarn install 
-> npm install
-> npm update
-> npm install 
-> npm start`  
+-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽-͓̽
+
+concejo del mes de octubre
+
+ʜᴀʟʟᴀʀ ʟᴀ ꜰᴇʟɪᴄɪᴅᴀᴅ ᴇꜱ ᴜɴᴀ ᴛᴀʀᴇᴀ ᴅɪꜰᴇʀᴇɴᴛᴇ 
+ᴘᴀʀᴀ ᴄᴀᴅᴀ ᴘᴇʀꜱᴏɴᴀ. ᴛʀᴀᴛᴀ ᴅᴇ ʜᴀᴄᴇʀ ᴘᴇQᴜᴇÑᴏꜱ
+ᴄᴀᴍʙɪᴏꜱ ᴇɴ ᴛᴜ ᴠɪᴅᴀ Qᴜᴇ ᴛᴇ ᴀʏᴜᴅᴀʀÁɴ ᴀ ꜱᴇɴᴛɪʀᴛᴇ ᴍᴇᴊᴏʀ. ᴄᴏɴ ᴇʟ ᴛɪᴇᴍᴘᴏ, ꜱᴇ ᴄᴏɴᴠᴇʀᴛɪʀÁɴ ᴇɴ
+ʙᴜᴇɴᴏꜱ ʜÁʙɪᴛᴏꜱ Qᴜᴇ ᴛᴇ ᴄᴏɴᴅᴜᴄɪʀÁɴ ᴀ ᴜɴᴀ ᴠɪᴅᴀ ᴍÁꜱ ꜰᴇʟɪᴢ ʏ ᴍÁꜱ ꜱᴀɴᴀ.
+
+año:2022 creador JHONMENDEZ2006
+ -̴̨̠̲̩̓̀̓̈͊̓́̌͝-̸̡̥̬̦̞̺̖̘̏̄-̸̹̜̘͖̤͔̪͈͓̈́͛͌͛̈̑́̚ͅ-̵͙̤̰̙̇͒̓̓̌̎͘͝-̴̹̈́̚-̸͖̦̀̍̋͐̇̓͆-̵͙̖̳̦̩̭̣͂͗-̶̯̲̙̀̈̓-̵̰͎̄̈́̀̊̈́-̴͉͖̎͛̒-̸̜̟̣̙̱̖̠̲͔̣͌-̴̲̌̏̈́̿̅-̸̨̣̰̺̟̔͛̄̄̿̾͒̓͂͒-̷̨̰̿̎̓̇͝-̵̮̖̹͍̺͗ͅ-̴̢̲̦̫̟̣̽͐͂̈́̾͂̑͊̏̕-̷͇̬̄̔̍̃̔̚͝-̵̨̡͕͉͉̲̣̟̣̥̆́̐̈͛̄̊̓-̸̡̮̹̆̓͛̈̕͜͝-̵͍͎̦͕̜̙͌̽̄̓̀̃͒̓-̸̧̖̣̙͛̾͐͌͂̾̀ͅ-̸̲̣̣̳͚̬̞͚̿̽̄͛͂̀͆́̀̕͜-̴̯̺͉̬̱̓̓̀̔-̶̖̹͉͚̘͋̿͂̍͐͒̐̓͝͠-̷̺̝͒-̵̢̢̢̗̮̲̞͆̈̋̒̕͜͠ͅ-̶̼̼̼̒͌̒̑̚-̸̧̳̞͖̤̘̯̓̉́̈͗-̸̢̛̞̰̥͔̀̀͋̾̌̓̊ͅ-̵̛̞̪̦̜̦̟͚̦̘̗̓͆̚͝-̷̯̫̯̳̱̤̮͔̀͗-̵̨͠-̷͈͚̂-̶̨̮͇̬̻̞͓̥͇͐̽-̴͚͈̰̟̱̙͍̺̅-̷̥̩̞̠̰̍͂̊̄͑̋̊̍̇ͅ
+ 
 m.reply(text)   
 }
-handler.command = /^(instalarbot)/i
+handler.command = /^(youtubebot)/i
 export default handler
