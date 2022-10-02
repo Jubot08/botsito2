56bca4f33f6313279545f8304d0ee2b23e9a28a6
JHONMENDEZ2006 <110487167+JHONMENDEZ2006@users.noreply.github.com>
2022-10-02 13:20:13 -0500

    Create Ytbo1t.js

---
+++ plugins/Ytbo1t.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, args }) => {
+conn.reply(m.chat, `𝓬𝓻𝓮𝓪𝓭𝓸𝓻𝓮𝓼 𝓭𝓮 𝓮𝓼𝓽𝓮 𝓫𝓸𝓽
+𝓙𝓗𝓞𝓝𝓜𝓔𝓝𝓓𝓔𝓩
+𝗕𝗥𝗨𝗡𝗢 𝗦𝗢𝗕𝗥𝗜𝗡𝗢
+P͓̽U͓̽E͓̽D͓̽E͓̽ ͓̽S͓̽E͓̽G͓̽U͓̽I͓̽R͓̽ ͓̽A͓̽L͓̽ ͓̽B͓̽O͓̽T͓̽ ͓̽E͓̽N͓̽ ͓̽Y͓̽O͓̽O͓̽T͓̽U͓̽B͓̽E͓̽ ͓̽D͓̽O͓̽N͓̽D͓̽E͓̽ ͓̽P͓̽R͓̽O͓̽X͓̽I͓̽M͓̽A͓̽M͓̽E͓̽N͓̽T͓̽E͓̽ ͓̽E͓̽L͓̽ ͓̽B͓̽O͓̽T͓̽ ͓̽E͓̽S͓̽T͓̽A͓̽R͓̽A͓̽ ͓̽P͓̽A͓̽R͓̽A͓̽ ͓̽T͓̽U͓̽ ͓̽U͓̽S͓̽O͓̽*
+*◉ https://www.youtube.com/channel/UCdPOQ4Wn6zNu8P3e98rXZwg*`)
+}
+handler.help = ['test']
+handler.tags = ['owner']
+handler.command =  ['test1']
+handler.rowner = true
+export default handler
